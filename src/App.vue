910af90d3337b8de4fb1b29b64465d2e4e9ea02e
eric <howcrazys@gmail.com>
2022-02-25 16:45:35 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,18 @@
   <router-view/>
 </template>
 
+<script>
+export default {
+  name:'App',
+  mounted(){
+    // sweet alert
+    // this.$swal('Hello Vue world!!!')
+  }
+}
+</script>
 <style lang="scss">
+@import '~bootstrap/dist/css/bootstrap.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'bootstrap'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import 'swiper/swiper.scss'
+
+
+const app = createApp(App)
+app.component('Swiper', Swiper)
+app.component('SwiperSlide', SwiperSlide)
+
+app.use(store).use(VueAxios, axios).use(router).use(VueSweetalert2).mount('#app')
